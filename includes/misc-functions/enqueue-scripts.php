8c1f1b68da8084f7c9063f1818151eef683875d3
Phil Johnston <johnstonphilip@gmail.com>
2014-01-17 13:54:43 -0500

    beta1.0.0.0

--- includes/misc-functions/enqueue-scripts.php
+++ includes/misc-functions/enqueue-scripts.php
@@ -17,9 +17,9 @@ add_action('admin_enqueue_scripts', 'mp_ecommerce_previews_admin_enqueue');
 function mp_ecommerce_previews_frontend_enqueue(){
 		
 		//front end js
-		wp_enqueue_script( 'mp_ecommerce_previews_front_end_js', plugins_url('js/mp-ecommerce-previews-front-end.js', dirname(__FILE__) ), array( 'jquery' ) );
+		//wp_enqueue_script( 'mp_ecommerce_previews_front_end_js', plugins_url('js/mp-ecommerce-previews-front-end.js', dirname(__FILE__) ), array( 'jquery' ) );
 		
 		//front end css
-		wp_enqueue_style( 'mp_ecommerce_previews_admin_style', plugins_url('css/mp-ecommerce-previews-admin-style.css', dirname(__FILE__)) );
+		//wp_enqueue_style( 'mp_ecommerce_previews_admin_style', plugins_url('css/mp-ecommerce-previews-admin-style.css', dirname(__FILE__)) );
 }
 add_action( 'wp_enqueue_scripts', 'mp_ecommerce_previews_frontend_enqueue' );
